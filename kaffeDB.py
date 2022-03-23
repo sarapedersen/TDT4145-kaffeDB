954746e7b3a5c1a9ab9136e3366a35d1ea3c31c9
thea <thea.kvinnegard@gmail.com>
2022-03-23 23:26:19 +0100

    fixed stuff

--- kaffeDB.py
+++ kaffeDB.py
@@ -12,7 +12,7 @@ cursor = con.cursor()
 # cursor.execute("INSERT INTO kaffesmaking VALUES (4, 'trekantet', 12, '23.03.2022', 'saraped@stud.ntnu.no', 'Brasil, Diamond Santos', 'Jacobsen og Svart')")
 # cursor.execute("INSERT INTO kaffesmaking VALUES (5, 'rund og skarp', 12, '22.03.2022', 'saraped@stud.ntnu.no', 'Espressokaffe', 'Kaffehuset Friele')")
 # cursor.execute("INSERT INTO ferdigbrentKaffe VALUES ('Frokostkaffe', 'Kaffehuset Friele', '20.03.2022', 'rund og fyldig kaffe med lang ettersmak', 144, 'lys', 100)")
-# cursor.execute("INSERT INTO ferdigbrentKaffe VALUES ('Kaffehuset Friele', 'mørk og god', '10.11.2021', 'Café Noir', 212, 'mørk', 100)")
+# cursor.execute("INSERT INTO ferdigbrentKaffe VALUES ('Kaffehuset Friele', 'Café Noir', '10.11.2021', 'mørk og god', 212, 'mørk', 100)")
 # cursor.execute("INSERT INTO ferdigbrentKaffe VALUES ('Espressokaffe', 'Kaffehuset Friele', '22.12.2021', 'flott espressokaffe med god fylde', 201, 'mørk', 100)")
 # cursor.execute("INSERT INTO ferdigbrentKaffe VALUES ('Honduras El Campo', 'Jacobsen og Svart ', '02.01.2022', 'søt, fruktig kaffe med smak av moden fersken, røde druer og fiken', 716, 'lys', 100)")
 # cursor.execute("INSERT INTO ferdigbrentKaffe VALUES ('Brasil, Diamond Santos', 'Jacobsen og Svart ', '22.02.2022', 'søt, fruktig kaffe med smak av moden fersken, røde druer og fiken', 398, 'mørk', 100)")
@@ -22,14 +22,12 @@ cursor = con.cursor()
 # cursor.execute("INSERT INTO kaffebønner VALUES ('Arabica', 'Coffea Arabica')")
 # cursor.execute("INSERT INTO kaffebønner VALUES ('Robusta', 'Coffea Robusta')")
 # cursor.execute("INSERT INTO kaffebønner VALUES ('Liberica', 'Coffea Liberica')")
+# cursor.execute("DELETE FROM ferdigbrentKaffe")
+# cursor.execute("SELECT * FROM ferdigbrentKaffe")
+# print(cursor.fetchall())
 
-
-
-counter = random.randrange(0, 10000, 1)
 bruker = "saraped@stud.ntnu.no"
 dagensDato = "23.03.2022"
-
-
 
 def brukerhistorie1(): 
     #  Input fra brukerens side er brenneri, kaffenavn, poeng og smaksnotat
@@ -42,7 +40,7 @@ def brukerhistorie1():
     smaksnotat = input("Smaksnotat: ")
     #bruker = input("Bruker: ")  
     poeng = int(poeng)
-    cursor.execute("INSERT INTO  kaffesmaking VALUES ({}, '{}', {}, '{}', '{}', '{}', '{}')".format(counter, smaksnotat, poeng, dagensDato, bruker, kaffeliste[kaffeid][1], kaffeliste[kaffeid][0]))
+    cursor.execute("INSERT INTO  kaffesmaking VALUES ({}, '{}', {}, '{}', '{}', '{}', '{}')".format(counter(), smaksnotat, poeng, dagensDato, bruker, kaffeliste[kaffeid][1], kaffeliste[kaffeid][0]))
     cursor.execute("SELECT * FROM kaffesmaking")
     print(cursor.fetchall())
 
@@ -64,30 +62,27 @@ def brukerhistorie3():
     # prisliste = cursor.fetchall()
     # for x in prisliste:
     print("hei")
-        
-    
 
-
-print("Velkommen til kaffeDB!") 
-harBruker = input("Har du bruker? (j/n) ")
-brukerliste = cursor.execute("SELECT epost, passord FROM bruker")
-if (harBruker == "j"): 
+def loggIn(): 
     riktigInfo = False
+    
     while riktigInfo == False: 
+        brukerliste = cursor.execute("SELECT epost, passord FROM bruker")
         epost = input("Epost: ")
         passord = input("Passord: ")
         for y in brukerliste: 
             if epost == y[0]:
-                print("runde 1")
                 if passord == y[1]: 
-                    bruker = epost 
-                    print("runde 2")
-                    break
-        print("Feil epost eller passord er oppgitt, prøv igjen")
-else: 
+                    riktigInfo == True
+                    return epost
+        print("Feil epost eller passord er oppgitt, prøv igjen") 
+
+
+def lagBruker(): 
     print("Du må lage bruker :D")
     epost = input("Epost: ")
     unik = False
+    brukerliste = cursor.execute("SELECT epost, passord FROM bruker")
     while unik == False:
         unik = True
         for x in brukerliste: 
@@ -104,18 +99,30 @@ else:
         passord1 = input("Passord: ") 
         passord2 = input("Bekreft passord: ")
     cursor.execute("INSERT INTO bruker VALUES ('{}', '{}', '{}', '{}')".format(epost, passord1, fornavn, etternavn))
-    bruker = epost
-    print("Gratulerer, du har laget bruker! Du er nå logget inn")   
-userInput = "null"
-while userInput != "exit" :
-    counter += 1
-    userInput = input()
-    if userInput == "1": 
-        brukerhistorie1()
-        print(userInput)
-    if userInput == "2": 
-        brukerhistorie2()
+    print("Gratulerer du har laget bruker!")
+    return epost
 
+def counter(): 
+    return random.randrange(0, 10000, 1)
+
+def main(): 
+    print("Velkommen til kaffeDB!") 
+    harBruker = input("Har du bruker? (j/n) ")
+    if harBruker == "j": 
+        bruker = loggIn()
+    else: 
+        bruker = lagBruker()
+    print("Du er nå logget inn")   
+    userInput = "null"
+    while userInput != "exit" :
+        userInput = input()
+        if userInput == "1": 
+            brukerhistorie1()
+            print(userInput)
+        if userInput == "2": 
+            brukerhistorie2()
+
+main()
 
 
 con.commit()
